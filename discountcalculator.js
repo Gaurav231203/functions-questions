d8d40fe756fd108b83f5be33f51188d8ae937f80
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-26 17:25:41 +0530

    Added new files

--- discountcalculator.js
+++ discountcalculator.js
@@ -4,9 +4,12 @@ function discountcalc(discount ){
     return function(price){
         let discountamount = (price*discount)/100;
           console.log("Discount Amount:", discountamount);
-         return price - discount;
+         return price - discountamount;
     }
 
 }
 const finaldiscount = discountcalc(10);
-console.log(finaldiscount(100));
+console.log(finaldiscount(1000));
+//Discount Amount: 100
+//990
+//PS C:\Users\bisht\OneDrive\Desktop\functionqns> 
