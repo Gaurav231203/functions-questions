ccdf6052149d42d542c01741259e5e2ea41377b5
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-28 11:21:00 +0530

    Added new files

---
+++ smlcalc.js
@@ -0,0 +1,14 @@
+ function calculatior(a, b){
+     let addition = a+b;
+     let subtraction = a - b ;
+    let multiply = a*b ;
+    let divide = a/b ;
+
+ console.log(addition);
+ console.log(subtraction);
+ console.log(multiply);
+ console.log(divide);
+
+ }
+ let fixedNumber = calculatior( 20, 10); 
+
