d685b69efe60a77fe7f00b8531a41ac567a7932c
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-28 10:42:47 +0530

    Added new files

---
+++ createobj.js
@@ -0,0 +1,11 @@
+/*let mobile = {
+    brand: "Samsung",
+    price: 25000
+};*/
+function knowaledge(){
+    let mobile = {
+        brand: "Samsung",
+        price: 25000
+    };
+    return mobile;
+}
---
+++ parameterqn.js
@@ -0,0 +1,5 @@
+function muktiply(a,b){
+    return a*b;
+
+}
+console.log(muktiply(2,10))
