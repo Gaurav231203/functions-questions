ad9ba281c37175318414bcb0e2ba4c830da77d89
GAURAV BISHT <bishtgaurav222@gmail.com>
2026-06-28 11:26:11 +0530

    Added new files

---
+++ add3num.js
@@ -0,0 +1,14 @@
+/*k function banao jo 3 numbers ka sum return kare.
+
+function sum(a, b, c){
+
+}
+
+console.log(sum(10, 20, 30));
+Expected Output
+60*/
+
+function Sum(a, b , c){
+    return a+b+c;
+}
+ console.log(Sum(10 , 20 , 30));
